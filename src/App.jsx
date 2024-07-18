b0d31ac489b8cecf20b622f7dd3ce3f053d4c350
CyberRazor <stomp224@gmail.com>
2024-07-18 16:27:34 +0100

    React app initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import TaskInput from './components/TaskInput'
+
+function App() {
+
+
+  return (
+    <>
+      <Header  />
+
+        <TaskInput/>
+    </>
+  )
+}
+
+export default App
